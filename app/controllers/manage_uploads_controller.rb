4e1b4e59a02f72cacbcbd88b35bd838ed42dec8b
ManojParmar-MTAJ <parmar.manoj@mtajsolutions.com>
2018-02-28 09:51:00 +0530

    flash messages added

--- app/controllers/manage_uploads_controller.rb
+++ app/controllers/manage_uploads_controller.rb
@@ -6,34 +6,34 @@ class ManageUploadsController < ApplicationController
 
   def upload_file_and_manage_users
     User.reject_if_unknown_file(files_from_params)
-    
     User.delay.create_from_file(collect_temp_files)
+    flash[:success] = 'Files Added for the proccessing successfully.'
     redirect_to root_path
   rescue => exception
     flash[:notice] = exception.message
-    render :index
-end
-
-def display_records
-end
-
-private
-
-def set_users
-  @users = User.all
-end
-
-def files_from_params
-  params.require(:excel_files)
-end
-
-def collect_temp_files
-  files_from_params.collect do |file|
-    tmp = file.tempfile
-    raise ToLargeFile.new("#{file.original_filename} is too large to handle.") if file.size > 10240000
-    destiny_file = File.join('public', 'uploads', file.original_filename)
-    FileUtils.move "//#{tmp.path}", destiny_file
-    destiny_file
+    redirect_to root_path
   end
-end
+
+  def display_records
+  end
+
+  private
+
+  def set_users
+    @users = User.all
+  end
+
+  def files_from_params
+    params.require(:excel_files)
+  end
+
+  def collect_temp_files
+    files_from_params.collect do |file|
+      tmp = file.tempfile
+      raise ToLargeFile.new("#{file.original_filename} is too large to handle.") if file.size > 10240000
+      destiny_file = File.join('public', 'uploads', file.original_filename)
+      FileUtils.move "//#{tmp.path}", destiny_file
+      destiny_file
+    end
+  end
 end
